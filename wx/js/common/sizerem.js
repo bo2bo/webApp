1c6092d1162b4170317ca1dbc24c56486ad8de2b
bo2bo <1290035233@qq.com>
2018-07-18 14:57:17 +0800

    init page

---
+++ wx/js/common/sizerem.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("title")[0].innerHTML = "智享微服务",
+    function () {
+        var e = document.documentElement.clientWidth / 750;
+        document.documentElement.style.fontSize = 100 * e + "px", document.querySelector(".yhfk") && (document.querySelector(".yhfk").style.display = "none")
+    }();
