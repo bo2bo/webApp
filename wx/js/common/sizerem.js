9bb07a6f981c1fc7172de2013fdd8eec3b4550ab
bo2bo <1290035233@qq.com>
2018-07-23 09:49:08 +0800

    修改文件目录

--- wx/js/common/sizerem.js
+++ wx/js/common/sizerem.js
@@ -1,5 +1,5 @@
-document.getElementsByTagName("title")[0].innerHTML = "智享微服务",
+document.getElementsByTagName("title")[0].innerHTML = "腾景网",
     function () {
         var e = document.documentElement.clientWidth / 750;
-        document.documentElement.style.fontSize = 100 * e + "px", document.querySelector(".yhfk") && (document.querySelector(".yhfk").style.display = "none")
+        document.documentElement.style.fontSize = 100 * e + "px";
     }();
